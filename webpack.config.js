0e0314d78f508341bbc76b5164e329a00dec99da
Norb48 <67010601+Norb48@users.noreply.github.com>
2021-04-07 12:57:42 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -70,6 +70,78 @@ module.exports = {
       template: path.resolve(__dirname, './src/one.html'),
       filename: 'one.html'
     }),
+     new HtmlWebpackPlugin({
+      title: 'two',
+      template: path.resolve(__dirname, './src/two.html'),
+      filename: 'two.html'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'tree',
+      template: path.resolve(__dirname, './src/tree.html'),
+      filename: 'tree.html'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'four',
+      template: path.resolve(__dirname, './src/four.html'),
+      filename: 'four.html'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'five',
+      template: path.resolve(__dirname, './src/five.html'),
+      filename: 'five.html'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'six',
+      template: path.resolve(__dirname, './src/six.html'),
+      filename: 'six.html'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'seven',
+      template: path.resolve(__dirname, './src/seven.html'),
+      filename: 'seven.html'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'eight',
+      template: path.resolve(__dirname, './src/eight.html'),
+      filename: 'eight.html'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'nine',
+      template: path.resolve(__dirname, './src/nine.html'),
+      filename: 'nine.html'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'ten',
+      template: path.resolve(__dirname, './src/ten.html'),
+      filename: 'ten.html'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'eleven',
+      template: path.resolve(__dirname, './src/eleven.html'),
+      filename: 'eleven.html'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'twelve',
+      template: path.resolve(__dirname, './src/twelve.html'),
+      filename: 'twelve.html'
+    }),
+
+    new HtmlWebpackPlugin({
+      title: 'thirteen',
+      template: path.resolve(__dirname, './src/thirteen.html'),
+      filename: 'thirteen.html'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'fourteen',
+      template: path.resolve(__dirname, './src/fourteen.html'),
+      filename: 'fourteen.html'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'mobile',
+      template: path.resolve(__dirname, './src/mobile.html'),
+      filename: 'mobile.html'
+    }),
+
   ],
   output: {
     path: path.resolve(__dirname, 'build'),
