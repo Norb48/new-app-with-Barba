0a9088119cc9c4d268a9305e2305738fe05b0413
Norb48 <67010601+Norb48@users.noreply.github.com>
2021-06-29 12:21:23 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,112 +1,199 @@
+import smoothscroll from 'smoothscroll-polyfill';
+import '../css/style.scss';
 import barba from '@barba/core';
 import barbaPrefetch from '@barba/prefetch';
 import gsap from 'gsap';
-import '../css/style.scss';
-// import lazyLoading from 'loading-attribute-polyfill';
+import CustomEase from "gsap/CustomEase";
+gsap.registerPlugin(CustomEase);
 import animOnes from './animOnes';
 import animEnter from './animEnter';
 import animLeave from './animLeave';
 import leaveToWork from './leaveToWork';
 import enterToWork from './enterToWork';
-import leaveToProject from './leaveToProjekt';
-import enterToProject from './enterToProjekt';
+import leaveToProject from './leavetoproject';
+import enterToProject from './entertoproject';
 import fromProjects from './fromprojects';
 import fromProjectsToWork from './fromprojectstowork';
 import fromNext from './fromnext';
 import toNext from './tonext';
 import * as myScroll from './myscroll';
-import * as slideIn from './intersection';
-import * as spiner from './loader';
+import * as slideInAnim from './intersection';
+// import * as spiner from './loader';
+import * as lazyLoadImg from './lazyloading';
+import * as prevent from './prevent';
+import * as rightNav from './rightnav';
+import * as arrow from './arrowdown';
+import lines from './lines'
+
+
 
 barba.use(barbaPrefetch);
 
 const reset = () => gsap.set('.scroll', {
   yPercent: 120,
 });
-
-const resetNav = () => gsap.set('.rightnav-link', {
-  autoAlpha: 0,
+const rightLinks = () => gsap.set('.rightnav-link', {
+  xPercent: 70,
 });
 
-barba.hooks.afterEnter(({ next }) => {
+
+
+barba.hooks.afterEnter(({
+  next
+}) => {
+  // spiner.init(next.container);
+  prevent.init(next.container);
   myScroll.init(next.container);
-   slideIn.init(next.container);
-  spiner.init(next.container);
+  rightNav.init(next.container);
+  slideInAnim.init(next.container);
+  lazyLoadImg.init(next.container);
+  arrow.init(next.container);
 });
+
 
 barba.init({
-  timeout: 5000,
   debug: true,
-  transitions: [
-    {
+  timeout: 5000,
+  cacheIgnore: false,
+  prefetchIgnore: false,
+
+  transitions: [{
       name: 'projects',
-      from:{
-        namespace:['work']
+      from: {
+        namespace: ['work']
       },
+      sync: true,
       leave: ({
         current
       }) => leaveToProject(current.container),
       to: {
-        namespace: ['one', 'two', 'tree', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'mobile'],
+        namespace: ['mcflurry', 'mcdonalds-drive-thru', 'bbb-21', 'social-media', 'ui', 'camapains', 'point', 'magazines', 'foxfilms', 'coca', 'pinacoteca', 'print', 'graphic', 'special', 'catalogs'],
       },
       enter: ({
         next
       }) => enterToProject(next.container),
     },
+
     {
-      name: 'leaveToProject',
+      name: 'leaveToWork',
       to: {
         namespace: ['work'],
       },
-      leave: ({ current }) => leaveToProject(current.container),
-      enter: ({ next }) => enterToProject(next.container),
-    }, {
+      sync: true,
+      leave: ({current}) => leaveToWork(current.container),
+
+      enter({next}) 
+      {
+        rightLinks();
+        gsap.to('.rightnav-link',{
+          duration: .3,
+          xPercent:0,
+          ease: CustomEase.create("inoutHoop", ".7,.1,.3,.9"),
+        }),
+        enterToWork(next.container)
+        // => enterToWork(next.container),
+    }
+    },
+
+    {
       name: 'default',
-      once({ next }) {
+      once({
+        next
+      }) {
         reset();
-        resetNav();
+        rightLinks();
 
         gsap.from('.leftnav-link', {
           duration: 0.5,
           autoAlpha: 0,
           stagger: 0.4,
-          ease: 'power1.in',
+          ease: CustomEase.create("inoutHoop", ".7,.1,.3,.9"),
           onComplete: () => animOnes(next.container),
         });
-      },
-      leave: ({ current }) => animLeave(current.container),
-      enter: ({ next }) => animEnter(next.container),
+      }
     },
-     {
+
+    {
+      name: 'contacts',
+      from: {
+        namespace: ['work', 'about', 'contact']
+      },
+      sync: true,
+      leave: ({
+        current
+      }) => animLeave(current.container),
+      enter: ({
+        next
+      }) => animEnter(next.container),  
+    },
+
+    {
+      name: 'lines',
+      to: {
+        namespace: ['contact']
+      },
+      sync: true,
+      leave: ({
+        current
+      }) => animLeave(current.container),
+      enter: ({
+        next
+      }) => lines(next.container),
+    },
+
+
+    {
       name: 'from-projects',
       from: {
-        namespace: ['one', 'two', 'tree', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'mobile'],
+        namespace: ['mcflurry', 'mcdonalds-drive-thru', 'bbb-21', 'social-media', 'ui', 'camapains', 'point', 'magazines', 'foxfilms', 'coca', 'pinacoteca', 'print', 'graphic', 'special', 'catalogs'],
       },
+
+      sync: true,
       leave: ({
         current
       }) => fromProjects(current.container),
       to: {
-        namespace: ['work'],
+        namespace: ['work', 'about'],
       },
       enter: ({
         next
       }) => fromProjectsToWork(next.container)
     },
-       {
+
+
+    {
       name: 'next',
       to: {
-        namespace: ['two', 'tree', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'mobile','one'],
+        namespace: ['mcdonalds-drive-thru', 'bbb-21', 'social-media', 'ui', 'camapains', 'point', 'magazines', 'foxfilms', 'coca', 'pinacoteca', 'print', 'graphic', 'special', 'mcflurry-easter', 'catalogs'],
       },
+      sync: true,
       leave: ({
         current
       }) => fromNext(current.container),
-    
       enter: ({
         next
       }) => toNext(next.container)
     },
+
   ],
 });
+
+
+smoothscroll.polyfill();
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+};
+
+
+
+
 
 
 
