fbbb1ff42332f3b9d1ff58ca9e88bea30db21277
Norb48 <67010601+Norb48@users.noreply.github.com>
2021-06-29 12:22:29 -0300

    Update animEnter.js

--- src/js/animEnter.js
+++ src/js/animEnter.js
@@ -1,23 +1,22 @@
 import gsap from 'gsap';
-
+import CustomEase  from "gsap/CustomEase";
+gsap.registerPlugin(CustomEase);
 
 function animEnter(container) {
-
-
-
  const main = container.querySelectorAll('.scroll')
-
- const tl = gsap.timeline({
-  defaults: { duration:.4, ease: 'power1.out' }
- });
-  
-
-  tl.fromTo(main,{xPercent: -110},{xPercent:0})
  
+ const tl = gsap.timeline({
+  defaults: {
+    duration: .7,
+    delay: .1,
+    ease: CustomEase.create("inoutHoop", ".7,.1,.3,.9")
+  }
+ });
+    
+  tl.fromTo(main,{xPercent: -110},{xPercent:0})
+   
 
-
-
- return tl
+return tl
 
 }
 export default animEnter
