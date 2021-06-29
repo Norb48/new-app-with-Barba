bfa3d170a642550c09bc23280a9f14b1a94a5d80
Norb48 <67010601+Norb48@users.noreply.github.com>
2021-06-29 12:30:15 -0300

    Update fromprojectstowork.js

--- src/js/fromprojectstowork.js
+++ src/js/fromprojectstowork.js
@@ -1,21 +1,22 @@
-import {
- gsap
-} from 'gsap';
-
+import {gsap} from 'gsap';
+import CustomEase from "gsap/CustomEase";
+gsap.registerPlugin(CustomEase);
 const fromProjectsToWork = (container) => {
 
  const main = container.querySelector('.scroll')
- const rightLinks = container.querySelectorAll('.rightnav-link')
+//  const rightLinks = container.querySelectorAll('.rightnav-link')
 
  const tl = gsap.timeline({
+
   defaults: {
-   duration: 0.5,
-   ease: 'power2.out'
-  }
+   duration: .8,
+   ease: CustomEase.create("inoutHoop", ".7,.1,.3,.9")
+   
+ }
  })
  tl.fromTo(main, {xPercent: -110}, {xPercent: 0})
-   .to(rightLinks, {autoAlpha: 1})
+//  tl.from(rightLinks, {autoAlpha: 0,stagger:{from:'start',amount:.5}},'-=0.22')
 
-return tl
+ return tl
 }
 export default fromProjectsToWork
