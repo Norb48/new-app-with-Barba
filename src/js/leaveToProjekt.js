49eb2a5591c81b1020dee26b5277bf60ac8fa194
Norb48 <67010601+Norb48@users.noreply.github.com>
2021-06-29 12:32:50 -0300

    Update leaveToProjekt.js

--- src/js/leaveToProjekt.js
+++ src/js/leaveToProjekt.js
@@ -1,14 +1,19 @@
 import gsap from 'gsap';
-
+import CustomEase from "gsap/CustomEase";
+gsap.registerPlugin(CustomEase);
 
 function leaveToProject(container) {
-const main = container.querySelectorAll('.scroll')
+  const main = container.querySelectorAll('.scroll')
+  const rightLinks = container.querySelectorAll('.rightnav-link')
+  const tl = gsap.timeline({
+    defaults: {
+      duration: .7,
+      ease: CustomEase.create("inoutHoop", ".7,.1,.3,.9")
+    }
+  });
 
- const tl = gsap.timeline({
-  defaults: { duration:0.4, ease: 'power1.in' }
- });
-  
-tl.fromTo(main,{xPercent: 0},{xPercent:-110})
- return tl
+  tl.to(rightLinks, {xPercent:-70,duration:0.15})
+    .fromTo(main, {xPercent: 0}, {xPercent: -110})
+  return tl
 }
 export default leaveToProject
