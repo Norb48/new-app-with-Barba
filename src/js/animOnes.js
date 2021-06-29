bd6dda04f502683c1172a09a6da0b5016be449b4
Norb48 <67010601+Norb48@users.noreply.github.com>
2021-06-29 12:24:46 -0300

    Update animOnes.js

--- src/js/animOnes.js
+++ src/js/animOnes.js
@@ -1,26 +1,40 @@
-import {gsap} from 'gsap';
-
+import {
+  gsap
+} from 'gsap';
+import CustomEase from "gsap/CustomEase";
+gsap.registerPlugin(CustomEase);
 
 
 const animOnes = (container) => {
 
- const rightLinks = container.querySelectorAll('.rightnav-link')
- const scroll = container.querySelector('.scroll')
+  const rightLinks = container.querySelectorAll('.rightnav-link')
+  const scroll = container.querySelector('.scroll')
   const h1 = container.querySelector('.section-link h1')
 
-const tl=gsap.timeline({
-defaults:{duration:.5}
-})
- tl 
+  const tl = gsap.timeline({
+    defaults: {
+      duration: 0.7
+    }
+  })
+  tl.to(rightLinks, {
+      xPercent: 0,
+      stagger: {
+        from: 'start',
+        amount: .5
+      }
+    }, '-=0.15')
+    .to(scroll, {
+      yPercent: 0,
+      ease: CustomEase.create("inoutHoop", ".7,.1,.3,.9")
+    }, .75)
+    .fromTo(h1, {
+      autoAlpha: 0
+    }, {
+      autoAlpha: 1,
+      ease: CustomEase.create("inoutHoop", ".8,.0,.2,1")
+    }, '-=0.25')
 
-
- .to(rightLinks,{autoAlpha:1, stagger:.09},'-=0.15')
- .to(scroll,{yPercent:0,ease:'power1.out'},1)
- .fromTo(h1,{autoAlpha:0},{autoAlpha:1,ease:'power2.out'},'-=0.25')
-
-
-
-   return tl
+  return tl
 
 }
 export default animOnes
