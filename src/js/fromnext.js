2ef3adbc0ff3e7ac1015ebb9bb0e180699d6a695
Norb48 <67010601+Norb48@users.noreply.github.com>
2021-06-29 12:27:43 -0300

    Update fromnext.js

--- src/js/fromnext.js
+++ src/js/fromnext.js
@@ -1,14 +1,15 @@
 import gsap from 'gsap';
-
+import CustomEase from "gsap/CustomEase";
+gsap.registerPlugin(CustomEase);
 
 function fromNext(container) {
 const main = container.querySelectorAll('.scroll')
 
  const tl = gsap.timeline({
-  defaults: { duration:0.38, ease: 'power1.out' }
+  defaults: { duration:0.8, ease: CustomEase.create("inoutHoop", ".7,.1,.3,.9")}
  });
   
-tl.fromTo(main,{scale:1},{scale:.92})
+tl.fromTo(main,{scale:1},{scale:.85})
  return tl
 }
 export default fromNext
