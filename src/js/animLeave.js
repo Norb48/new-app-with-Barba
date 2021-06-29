a9833003a3c6731348617b458287aa5d8267e0de
Norb48 <67010601+Norb48@users.noreply.github.com>
2021-06-29 12:23:55 -0300

    Update animLeave.js

--- src/js/animLeave.js
+++ src/js/animLeave.js
@@ -1,14 +1,18 @@
 import {gsap} from 'gsap';
-
+import CustomEase from "gsap/CustomEase";
+gsap.registerPlugin(CustomEase);
 const animLeave = (container) => {
 
   const main = container.querySelector('.scroll')
   const rightLinks = container.querySelectorAll('.rightnav-link')
   const tl=gsap.timeline({
-    defaults:{duration:0.4,ease:'power1.in'}
+    defaults: {
+      duration: .7,
+      ease: CustomEase.create("inoutHoop", ".7,.1,.3,.9")
+    }
   })
-  tl.to(rightLinks,{autoAlpha:0})
-    .to(main,{xPercent:110},'+=0.2')
+  tl.fromTo(rightLinks,{xPercent:0}, {xPercent:70,duration:0.15})
+  tl.fromTo(main,{xPercent:0}, {xPercent:110})
     
        return tl
 }
