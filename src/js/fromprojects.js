1b15b06175841a6c569d25110dbfba05114647c4
Norb48 <67010601+Norb48@users.noreply.github.com>
2021-06-29 12:28:41 -0300

    Update fromprojects.js

--- src/js/fromprojects.js
+++ src/js/fromprojects.js
@@ -1,13 +1,20 @@
 import gsap from 'gsap';
-
+import CustomEase from "gsap/CustomEase";
+gsap.registerPlugin(CustomEase);
 
 function fromProjects(container) {
-const main = container.querySelectorAll('.scroll')
+ const main = container.querySelectorAll('.scroll')
+
  const tl = gsap.timeline({
-  defaults: { duration:0.5, ease: 'power2.in' }
+
+  defaults: {
+   duration: 0.7,
+   ease: CustomEase.create("inoutHoop", ".7,.1,.3,.9")
+   
+ }
  });
-  
-tl.fromTo(main,{xPercent: 0},{xPercent:110})
+
+ tl.fromTo(main,{xPercent: 0}, {xPercent: 110})
 
  return tl
 }
